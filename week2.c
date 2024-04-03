ac8635b917d1d91e356d0801609a12f0bcd0b7e2
Yui0126 <yui.naruse.94@gmail.com>
2024-04-03 22:01:16 +0900

    added files

---
+++ week2.c
@@ -0,0 +1,31 @@
+# include <stdio.h>
+/*I am a comment*/
+
+int main() {
+    printf("Hello World\n");
+    printf("Answer %d\n", 42);
+    printf("Name %s\n", "Sarah");
+    printf("x %.1f i %d\n", 3.5, 100);
+}
+
+// int main() {
+//     int usf, euf;
+//     print("Enter US Floor\n");
+//     scanf("%d", &usf);
+//     euf = usf - 1;
+//     print("EU Floor %d\n", euf);
+// }
+
+// int main() {
+//     char name[100];
+//     printf("Enter name\n");
+//     scanf("%100s", name);
+//     printf("Hello %s\n", name);
+// }
+
+// int main() {
+//     char line[1000];
+//     printf("Enter line\n");
+//     scanf("%[^\n]1000s", line);
+//     printf("Line: %s\n", line);
+// }
